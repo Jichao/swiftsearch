244cf8cc4ed2c3331a2afa8318793ef880c4b6a1
  <>
2015-01-25 02:46:09 -0800

    Rename back to SwiftSearch

--- stdafx.cpp
+++ stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// SwiftSearch2.pch will be the pre-compiled header
+// SwiftSearch.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
